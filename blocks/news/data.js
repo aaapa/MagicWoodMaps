fa78bd725ccb730cbacc138651377baf28fd3b7d
Ramil <ramil26102006@gmail.com>
2024-06-22 18:38:43 +0300

    sample news page is ready

--- blocks/news/data.js
+++ blocks/news/data.js
@@ -4,37 +4,37 @@ export const data = {
       image: "https://i.ibb.co/TbJnxRz/comments-1.webp",
       date: "2024-06-22",
       title: "Чтение книг всегда делает моменты счастливыми",
-      link: "#!",
+      link: "example-news.html",
     },
     {
       image: "https://i.ibb.co/nP0rtBV/comments-2.webp",
       date: "2024-06-22",
       title: "Чтение книг всегда делает моменты счастливыми",
-      link: "#!",
+      link: "example-news.html",
     },
     {
       image: "https://i.ibb.co/TbJnxRz/comments-1.webp",
       date: "2024-06-22",
       title: "Чтение книг всегда делает моменты счастливыми",
-      link: "#!",
+      link: "example-news.html",
     },
     {
       image: "https://i.ibb.co/nP0rtBV/comments-2.webp",
       date: "2024-06-22",
       title: "Чтение книг всегда делает моменты счастливыми",
-      link: "#!",
+      link: "example-news.html",
     },
     {
       image: "https://i.ibb.co/TbJnxRz/comments-1.webp",
       date: "2024-06-22",
       title: "Чтение книг всегда делает моменты счастливыми",
-      link: "#!",
+      link: "example-news.html",
     },
     {
       image: "https://i.ibb.co/nP0rtBV/comments-2.webp",
       date: "2024-06-22",
       title: "Чтение книг всегда делает моменты счастливыми",
-      link: "#!",
+      link: "example-news.html",
     }
   ]
 };
